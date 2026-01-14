33c925d0498f3dc1bca002f74a6f8837b4683624
David Hernández Contreras <100537779+DavidHecon@users.noreply.github.com>
2026-01-14 08:05:10 -0600

    Delete utils directory

--- utils/pushGTMEven.js
+++
@@ -1,8 +0,0 @@
-export const pushGTMEvent = (eventName, eventData = {}) => {
-    if (typeof window !== 'undefined' && window.dataLayer) {
-      window.dataLayer.push({
-        event: eventName,
-        ...eventData,
-      });
-    }
-  };
